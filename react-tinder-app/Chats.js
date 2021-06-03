f9ca6f21f57cc325b3538dbbc9edc37ddf8e2695
Arad <aradkotzer@gmail.com>
2021-06-03 17:32:53 +0300

    arad chats changes

--- react-tinder-app/Chats.js
+++ react-tinder-app/Chats.js
@@ -1,6 +1,20 @@
 import React from "react";
 
-function Chats() {
-
+function Chats({name,message,timestamp,profilePic}) {
+    return (
+        <div>
+            <Link to={`chat/${name}`}>
+            <div className="chat">
+            <Avatar className="chat_image" alt={name} src={profilePic}/>
+            <div className="chat_details">
+                <h4>{name}</h4>
+    <p>{message}</p>
+            </div>
+    <p>{timestamp}</p>
+    </div>
+    </Link>
+        </div>
+    )
 }
-export default Chats;
+
+export default Chats
