9267b44afa9ea483124f93f3ebb2c773581e9063
Rgaibel <rgaibel@gmail.com>
2021-06-03 15:01:06 +0300

    my changes

--- pet-tinder/src/App.js
+++ pet-tinder/src/App.js
@@ -1,19 +1,19 @@
-import './App.css';
+import React from "react";
 import Header from './Header';
+import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>
-        "lets go team 36"
-        <Header></Header>
-        {/*header buttons*/}
-        {/*Tinder Cards */}
-        {/*buttons bellow tinder cards */}
+      <h1> "lets go team 36"</h1>
+      <Header/>
+      {/*header buttons*/}
+      {/*Tinder Cards */}
+      {/*buttons bellow tinder cards */}
 
-        {/*chat screen */}
-        {/*individual chat screen */}
-      </h1>
+      {/*chat screen */}
+      {/*individual chat screen */}
     </div>
   );
 }
---
+++ pet-tinder/src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./Header.css";
+
+
+function Header(){
+    return ( 
+        <div className="header"> 
+            <svg class="MuiSvgIcon-root jss173" focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 3c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3zm0 14.2c-2.5 0-4.71-1.28-6-3.22.03-1.99 4-3.08 6-3.08 1.99 0 5.97 1.09 6 3.08-1.29 1.94-3.5 3.22-6 3.22z"></path></svg>
+            <img src="https://www.infometrics.co.nz/wp-content/uploads/2020/11/Dog_Breeds-825x510.jpg" ></img>
+            <svg class="MuiSvgIcon-root jss173" focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path d="M20 2H4c-1.1 0-2 .9-2 2v18l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2z"></path></svg>
+        </div>
+    )
+}
+
+export default Header;
