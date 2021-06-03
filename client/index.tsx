2af0fa97532159dcb547049e2fa5e6120c080a1c
Omer MarChaim <omermar7@gmail.com>
2021-06-03 10:35:34 +0300

    hello world test

---
+++ client/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+render(
+    <h1>Hello, worlrd!</h1>,
+    document.getElementById('root')
+  );
+
+function render(arg0: any, arg1: HTMLElement) {
+    throw new Error('Function not implemented.');
+}
